64bd044663b25a31fd882bb153b41030c71524df
Eliabe Gai <eliabegai@gmail.com>
2025-02-24 11:22:15 -0300

    feat: style

--- src/cliente/dto/create-cliente.dto.ts
+++ src/cliente/dto/create-cliente.dto.ts
@@ -1,1 +1,16 @@
-export class CreateClienteDto {}
+import { IsString, IsEmail, IsOptional } from 'class-validator';
+
+export class CreateClienteDto {
+  @IsString()
+  nome: string;
+
+  @IsString()
+  telefone: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  protocolo: string;
+}
