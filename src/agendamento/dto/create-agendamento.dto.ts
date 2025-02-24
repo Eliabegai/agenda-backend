6d24c09e4012d55fd8104f72f53419bbffb3078e
Eliabe Gai <eliabegai@gmail.com>
2025-02-24 11:19:07 -0300

    feat: create cliente

--- src/agendamento/dto/create-agendamento.dto.ts
+++ src/agendamento/dto/create-agendamento.dto.ts
@@ -1,1 +1,16 @@
-export class CreateAgendamentoDto {}
+import { IsInt, IsString, IsDateString, IsOptional } from 'class-validator';
+
+export class CreateAgendamentoDto {
+  @IsInt()
+  funcionarioId: number;
+
+  @IsInt()
+  protocoloId: number;
+
+  @IsDateString()
+  dataHora: string;
+
+  @IsOptional()
+  @IsString()
+  status?: string;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { FuncionarioModule } from './funcionario/funcionario.module';
 import { AgendamentoModule } from './agendamento/agendamento.module';
+import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
-  imports: [AdminModule, FuncionarioModule, AgendamentoModule],
+  imports: [AdminModule, FuncionarioModule, AgendamentoModule, ClienteModule],
   controllers: [AppController],
   providers: [AppService],
 })
